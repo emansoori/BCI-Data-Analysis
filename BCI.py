2c4c2278eb149f71692f1fbc3727a8bb6aa4af1d
Erfan Mansoori <e.mansoori@mun.ca>
2023-11-19 12:31:46 -0330

    Update BCI.py

--- BCI.py
+++ BCI.py
@@ -1,13 +1,58 @@
 import mne
-import time
-import time
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import welch
 
+Data = mne.io.read_epochs_eeglab('E:\DATA Hadi\Data\Data\Recordings\Phase 1\PreProcessedData\P2\P2.set')
 
-
-Data = mne.io.read_epochs_eeglab('d:\Data\Recordings\Phase 1\PreProcessedData\P2 Processed\P2.set')
 df = Data.to_data_frame()
-x = df[df.columns[3:4]].to_numpy()
-x = x[:1123]
-x = np.transpose(x)
-print (x.shape)
+
+
+T7_Electrode = df[df.columns[9:10]].to_numpy()[:1123]
+O2_Electrode = df[df.columns[18:19]].to_numpy()[:1123]
+
+
+plt.figure(figsize=(10, 8))
+
+
+plt.subplot(2, 1, 1)
+plt.plot(T7_Electrode, label='T7')
+plt.xlabel('Sample Number')
+plt.ylabel('Amplitude')
+plt.title('EEG Data - Channel T7')
+plt.legend()
+
+
+plt.subplot(2, 1, 2)
+plt.plot(O2_Electrode, label='O2', color='red')
+plt.xlabel('Sample Number')
+plt.ylabel('Amplitude')
+plt.title('EEG Data - Channel O2')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+
+plt.figure(figsize=(10, 8))
+
+
+plt.subplot(2, 1, 1)
+frequencies, psd_t7 = welch(T7_Electrode[:, 0], fs=Data.info['sfreq'], nperseg=256, scaling='density')
+plt.semilogy(frequencies, psd_t7, color='b', linestyle='-', label='T7 PSD')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Power/Frequency (dB/Hz)')
+plt.title('Power Spectral Density (PSD) - T7')
+plt.legend()
+
+
+plt.subplot(2, 1, 2)
+frequencies, psd_o2 = welch(O2_Electrode[:, 0], fs=Data.info['sfreq'], nperseg=256, scaling='density')
+plt.semilogy(frequencies, psd_o2, color='r', linestyle='-', label='O2 PSD')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Power/Frequency (dB/Hz)')
+plt.title('Power Spectral Density (PSD) - O2')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
